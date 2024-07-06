2cbd2f5a51272fa3d17e514480021a69b5608822
AishwaryaSubash <riya129ash@gmail.com>
2024-07-06 18:04:31 +0530

    config done

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+const router = express.Router();
+dotenv.config();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.listen(process.env.PORT, function () {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello there!");
+});
